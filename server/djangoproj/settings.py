c6cb943bd9515896049bdd370d2c9e4773422c79
4LPH4-1 <sneculai71@gmail.com>
2024-08-10 06:11:48 -0400

    Changed links

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "https://ahmadalievsd-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    "https://sneculai71-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://ahmadalievsd-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    "https://sneculai71-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"
 ]
 
 REST_FRAMEWORK = {
@@ -140,6 +140,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/static"),
-    os.path.join(BASE_DIR, "frontend/build"),
-    os.path.join(BASE_DIR, "frontend/build/static"),
+    
 ]
