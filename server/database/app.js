1206f1d7c1daefad93efd7714a15cd5a7a8c52d2
4LPH4-1 <153223057+4LPH4-1@users.noreply.github.com>
2024-09-09 11:11:26 +0300

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -58,34 +59,18 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-  try {
-    const documents = await Dealerships.find();
-    res.json(documents);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
-  }
+//Write your code here
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-  try {
-    const documents = await Dealerships.find({state: req.params.state});
-    res.json(documents);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
-  }
+//Write your code here
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-  try {
-    const documents = await Dealerships.find({id: req.params.id});
-    res.json(documents);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
-  }
+//Write your code here
 });
-
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
@@ -113,7 +98,6 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
     res.status(500).json({ error: 'Error inserting review' });
   }
 });
-
 
 // Start the Express server
 app.listen(port, () => {
